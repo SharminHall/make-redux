f6620495f3e534d8d0051ef5a537ecba43af903f
sharmin <sharminh@163.com>
2019-04-25 18:23:49 +0800

    create

--- src/index.js
+++ src/index.js
@@ -21,21 +21,82 @@ const appState = {
   }
 }
 
-function renderApp (appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
+function dispatch (state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      state.title.text = action.text
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
+    case 'UPDATE_TITLE_COLOR':
+      state.title.color = action.color
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    default:
+      return state
+  }
 }
 
-function renderTitle (title) {
+function createStore (state, stateChanger) {
+  // 订阅-监听
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+
+  const getState = () => state
+  // 每次更改state时，就会触发所有listener
+  const dispatch = (action) => {
+    state = stateChanger(state, action)
+    listeners.forEach(listener => {
+      listener()
+    })
+  }
+  return {getState, dispatch, subscribe}
+}
+
+function renderApp (state, oldState = {}) {
+  if (state === oldState) return
+  console.log('render app ------------------------------')
+  renderTitle(state.title, oldState.title)
+  renderContent(state.content, oldState.content)
+  console.log('end ------------------------------')
+}
+
+function renderTitle (title, oldTitle = {}) {
+  if (title === oldTitle) return
+  console.log('render title')
   const titleDOM = document.getElementById('title')
   titleDOM.innerHTML = title.text
   titleDOM.style.color = title.color
 }
 
-function renderContent (content) {
+function renderContent (content, oldContent = {}) {
+  if (content === oldContent) return
+  console.log('render content')
   const contentDOM = document.getElementById('content')
   contentDOM.innerHTML = content.text
   contentDOM.style.color = content.color
 }
 
-renderApp(appState)
+const store = createStore(appState, dispatch)
+let oldState = store.getState()
+// 订阅
+store.subscribe(() => {
+  let newState = store.getState()
+  renderApp(newState, oldState)
+  // 更新state后保存为旧状态，待下次更新
+  oldState = store.getState()
+})
+renderApp(store.getState())
+setTimeout(() => {
+  store.dispatch({type: 'UPDATE_TITLE_TEXT', text: 'demo title'})
+  store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'black'})
+}, 3000)
